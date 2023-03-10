41d3a5deb6a22c0df90c0446d6ff462868fd5177
Ruthie Irvin <ruth.diane.irvin@gmail.com>
2023-03-10 01:32:44 -0600

    adds decoding for answer choices

--- client/src/components/questioncard.js
+++ client/src/components/questioncard.js
@@ -3,8 +3,8 @@ import { decode } from "html-entities";
 const QuestionCard = (props) => {
   //creates basic array of all 4 answer choices for use in the map
   let answers = [
-    props.question.correct_answer,
-    ...props.question.incorrect_answers,
+    decode(props.question.correct_answer),
+    decode(...props.question.incorrect_answers),
   ];
   // console.log(answers);
   //many thanks to Camille for sharing code with me so I could wrap my brain around how to map these multiple choice answers
