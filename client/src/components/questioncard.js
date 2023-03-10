2574e1d8c355f55dfd77ab4738c77077d69487ae
Ruthie Irvin <ruth.diane.irvin@gmail.com>
2023-03-10 00:27:48 -0600

    plays with CSS

--- client/src/components/questioncard.js
+++ client/src/components/questioncard.js
@@ -9,7 +9,7 @@ const QuestionCard = (props) => {
     <div className={"question-section"}>
       <div className="question-text">{decode(props.question.question)}</div>
       <div className="answer-section">{answers.map((answer, index) => {
-        return <button key={index} name="answerChoices" onClick={()=>{console.log("working")}}>{answer}</button>
+        return <button className="button-52" key={index} name="answerChoices" onClick={()=>{console.log("working")}}>{answer}</button>
       })}
       </div>
     </div>
