105a9a0edd5636b3ddfa5671594eb9f44475bf76
Ruthie Irvin <ruth.diane.irvin@gmail.com>
2023-03-09 23:22:53 -0600

    decodes the html

--- server/index.js
+++ server/index.js
@@ -3,8 +3,9 @@ import bodyParser from 'body-parser';
 import cors from 'cors';
 import fetch from 'node-fetch';
 
+
 // For testing my API I saving one response in a .js file 
-import fakedata  from './fakedata.js';
+//import fakedata  from './fakedata.js';
 
 const app = express();
 const PORT = 5000;
@@ -28,14 +29,14 @@ app.get("/", (req, res) => {
 
   // //hardcode the game response for testing reasons to don't saturate my API call. 
 app.get('/api/game', (req, res) =>{
-  fetch("https://opentdb.com/api.php?amount=10")
+  fetch("https://opentdb.com/api.php?amount=10&difficulty=easy&type=multiple")
   .then((response) => response.json())
   // the below line of code doesn't in this case actually need the curly brackets and return statement here because there's only one thing happening
   //but I'm leaving it in to remember the Lesson with Natalia about how I could make more than one thing happen in this .then statment
   .then((triviaData) => {
     //reverse alphabetize answers so true always comes before false
     //transform each result in a certain way - modify triviaData.results to include another property called answers which makes one array of strings holding all the answers
-    
+
     
     //triviaData before:    
 
